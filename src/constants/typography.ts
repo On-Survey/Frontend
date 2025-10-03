b8eed0f6d812f6df0eb036694de2b97c41e87ec6
chldsbdud <cyy2532@gmail.com>
2025-10-03 15:22:57 +0900

    🔧 Chore : 폴더 구조 생성

---
+++ src/constants/typography.ts
@@ -0,0 +1,10 @@
+// 폰트
+export const TYPOGRAPHY = {
+  heading: {
+    h1: { fontSize: "2.25rem", lineHeight: "2.5rem", fontWeight: "700" }, // text-4xl
+    h2: { fontSize: "1.875rem", lineHeight: "2.25rem", fontWeight: "600" }, // text-3xl
+    h3: { fontSize: "1.5rem", lineHeight: "2rem", fontWeight: "600" }, // text-2xl
+  },
+  body: {},
+  fontFamily: {},
+};
