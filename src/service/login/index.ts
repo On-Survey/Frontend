8edd09559239ab30a00b70fb783349474d14d9b6
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-10-21 23:47:22 +0900

    ✨ Feat: 로그인 후 토큰 추출 로직 수정
    feat: 로그인 후 토큰 추출 로직 수정

--- src/service/login/index.ts
+++ src/service/login/index.ts
@@ -14,9 +14,11 @@ export const loginApi = async (
 		referrer,
 	});
 
+	console.log(response);
+
 	// 헤더에서 토큰 추출
-	const authorizationHeader = response.headers.Authorization;
-	const refreshTokenHeader = response.headers.XRefreshToken;
+	const authorizationHeader = response.headers.authorization;
+	const refreshTokenHeader = response.headers["x-refresh-token"];
 
 	if (!authorizationHeader || !refreshTokenHeader) {
 		throw new Error("토큰 추출 실패");
