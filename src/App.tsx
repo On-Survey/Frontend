a3f92bf6626a179869787745f1d54329399e3e06
chldsbdud <cyy2532@gmail.com>
2025-10-24 23:50:44 +0900

    fix: App.tsx에 /home 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { Create } from "./pages/Create";
+import { Home } from "./pages/Home";
 import { Intro } from "./pages/Intro";
 import { Main } from "./pages/Main";
 import { Onboarding } from "./pages/Onboarding";
@@ -10,6 +11,7 @@ export const App = () => {
 		<Router>
 			<Routes>
 				<Route path="/" element={<Intro />} />
+				<Route path="/home" element={<Home />} />
 				<Route path="/onboarding" element={<Onboarding />} />
 				<Route path="/main" element={<Main />} />
 				<Route path="/create" element={<Create />} />
