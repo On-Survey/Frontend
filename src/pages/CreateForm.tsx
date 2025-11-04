9d22dcfdb3a5c3015a50000d7971a05802e8e5d5
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-11-04 17:51:23 +0900

    ✨ Feat: 관심사 선택 페이지 퍼블리싱
    feat: 관심사 선택 페이지 퍼블리싱

--- src/pages/CreateForm.tsx
+++ src/pages/CreateForm.tsx
@@ -2,25 +2,28 @@ import { ProgressStep, ProgressStepper } from "@toss/tds-mobile";
 import { useCreateForm } from "../contexts/CreateFormContext";
 import FormTitleStep from "./form/FormTitleStep";
 import QuestionHome from "./form/QuestionHome";
+import InterestPage from "./interest/InterestPage";
 import ScreeningQuestion from "./screening/ScreeningMain";
 
 export const CreateForm = () => {
 	const { activeStep } = useCreateForm();
 
 	return (
-		<div className="min-h-screen flex items-center flex-col">
-			<ProgressStepper variant="compact" activeStepIndex={activeStep}>
-				<ProgressStep />
-				<ProgressStep />
-				<ProgressStep />
-				<ProgressStep />
-				<ProgressStep />
-			</ProgressStepper>
-			<div className="flex flex-col w-full px-1 gap-4 mt-3">
+		<div className="min-h-screen flex flex-col">
+			<div className="sticky top-0 z-50 w-full bg-white">
+				<ProgressStepper variant="compact" activeStepIndex={activeStep}>
+					<ProgressStep />
+					<ProgressStep />
+					<ProgressStep />
+					<ProgressStep />
+					<ProgressStep />
+				</ProgressStepper>
+			</div>
+			<div className="flex flex-col w-full px-1 gap-4 mt-3 flex-1 overflow-y-auto">
 				{activeStep === 0 && <FormTitleStep />}
 				{activeStep === 1 && <QuestionHome />}
 				{activeStep === 2 && <ScreeningQuestion />}
-				{activeStep === 3 && <div>Step 4</div>}
+				{activeStep === 3 && <InterestPage />}
 				{activeStep === 4 && <div>Step 5</div>}
 			</div>
 		</div>
